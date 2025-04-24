af179bc4ce784c999e1eaa0a62141fdcee56fdcf
Wendy <wendyshen2005@gmail.com>
2025-04-24 18:03:41 -0400

    completed application frontend

--- app/app.js
+++ app/app.js
@@ -3,7 +3,29 @@
 var touchstoneTest = angular.module('touchstoneTest', [
     'ngRoute',
     'ngMaterial'
-]);
+]).directive('myInput', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            label: '@',
+            model: '=',
+            type: '@',
+            checkVal: '&?'
+        },
+        template: "<md-input-container>\n                    <label>{{label}}</label>\n                    <input ng-change=\"checkVal({val:model})\" ng-attr-type=\"{{type}}\" ng-model=\"model\" required=\"\">\n                </md-input-container>"
+    };
+}).directive('mySelect', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            label: '@',
+            model: '=',
+            options: '=',
+            checkVal: '&?'
+        },
+        template: "<md-input-container>\n                        <label>{{label}}</label>\n                        <md-select ng-model=\"model\" ng-change=\"checkVal({val:model})\">\n                            <md-option ng-repeat=\"opt in options\" value=\"{{opt}}\">\n                                {{opt}}\n                            </md-option>\n                        </md-select>\n                    </md-input-container>"
+    };
+});
 //stuff that run before this is loaded/running
 touchstoneTest.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
@@ -24,34 +46,77 @@ touchstoneTest.controller('touchstoneTestController', ['$scope', function ($scop
         //defines a var called message
         //when message is called in view, will show hey y'all
         //as long as message is called within the element scope
-        $scope.ninjas = [
-            {
-                name: "yoshi",
-                belt: "green",
-                rate: 50,
-                available: true
-            }, {
-                name: "crystal",
-                belt: "green",
-                rate: 30,
-                available: true
-            }, {
-                name: "shaun",
-                belt: "green",
-                rate: 10,
-                available: false
-            }, {
-                name: "jennie",
-                belt: "green",
-                rate: 30,
-                available: false
-            }, {
-                name: "lima",
-                belt: "green",
-                rate: 1000,
-                available: true
+        $scope.checkVal = function (val) {
+            // check that data is updated correctly
+            console.log(val);
+            console.log($scope.appInfo);
+        };
+        $scope.appInfo = {
+            personalInfo: {
+                flags: [],
+                firstName: null,
+                lastName: null,
+                email: null,
+                birthday: null,
             },
-        ];
+            legalStat: {
+                flags: [],
+                legalStatus: null,
+                drivers: {
+                    hasDrivers: null,
+                    driversType: null,
+                },
+                numPracticeHours: null,
+                prevPRAAttempts: [],
+            },
+            //written TDM, result, is current?
+            medicalEd: {
+                flags: [],
+                school: null,
+                medDegreeName: null,
+                gradYr: null,
+                edLang: null,
+            },
+            engProficiency: {
+                flags: [],
+                test: null,
+                score: null,
+                expired: null,
+                exceeded: null,
+                activeUse: null
+            },
+            exams: {
+                flags: [],
+                NACDate: null,
+                MCCQE2Date: null,
+                MCCQE1Date: null,
+            },
+            postGradTraining: {
+                flags: [],
+                monthsPostGrad: null,
+                monthsIndependent: null,
+                completed2Yr: null
+            },
+            rotations: {
+                flags: [],
+                completed7: null,
+                impairment: null
+            }
+        };
+        $scope.addPrevPRA = function () {
+            $scope.checkVal(1);
+            console.log("Added PRA");
+            $scope.appInfo.legalStat.prevPRAAttempts.push({
+                written: null,
+                passed: null,
+                current: null
+            });
+        };
+        $scope.delPrevPRA = function (i) {
+            $scope.checkVal(1);
+            console.log("Deleted PRA");
+            $scope.appInfo.legalStat.prevPRAAttempts.splice(i, 1);
+        };
         $scope.engProficiency = [
             "0 - No experience",
             "1 - Beginner",
@@ -62,5 +127,13 @@ touchstoneTest.controller('touchstoneTestController', ['$scope', function ($scop
         $scope.immgStat = [
             "Canadian Citizen / Permeaneant Resident",
             "Neither"
+        ];
+        $scope.trueFalse = [true, false];
+        $scope.driversType = [
+            "Canadian",
+            "International"
+        ];
+        $scope.engTest = [
+            "IELTS", "OET", "CELPIP", "Recent practice in English speaking country"
         ];
     }]);
--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,42 @@
 /// <reference types="angular" />
 /// <reference types="angular-route" />
-
+ 
 var touchstoneTest = angular.module('touchstoneTest', [
     'ngRoute',
     'ngMaterial'
-]);
-
+]).directive('myInput', function() {
+    return {
+        restrict:'E',
+        scope: {
+            label:'@',
+            model:'=',
+            type:'@',
+            checkVal: '&?'
+        },
+        template:`<md-input-container>
+                    <label>{{label}}</label>
+                    <input ng-change="checkVal({val:model})" ng-attr-type="{{type}}" ng-model="model" required="">
+                </md-input-container>`
+    };
+}).directive('mySelect', function(){
+    return {
+        restrict:'E',
+        scope: {
+            label:'@',
+            model:'=',
+            options:'=',
+            checkVal: '&?'
+        },
+        template: `<md-input-container>
+                        <label>{{label}}</label>
+                        <md-select ng-model="model" ng-change="checkVal({val:model})">
+                            <md-option ng-repeat="opt in options" value="{{opt}}">
+                                {{opt}}
+                            </md-option>
+                        </md-select>
+                    </md-input-container>`
+    }
+});
 
 //stuff that run before this is loaded/running
 touchstoneTest.config( ['$routeProvider', function($routeProvider: { when: (arg0: string, arg1: { templateUrl: string; }) => { (): any; new(): any; when: { (arg0: string, arg1: { templateUrl: string; }): { (): any; new(): any; otherwise: { (arg0: { redirectTo: string; }): void; new(): any; }; }; new(): any; }; }; }){
@@ -32,35 +63,81 @@ touchstoneTest.controller('touchstoneTestController', ['$scope', function($scope
     //defines a var called message
     //when message is called in view, will show hey y'all
     //as long as message is called within the element scope
-    $scope.ninjas = [
-        {
-            name: "yoshi",
-            belt:"green",
-            rate: 50,
-            available:true
-        },{
-            name: "crystal",
-            belt:"green",
-            rate: 30,
-            available:true
+    $scope.checkVal = (val: string) => {
+        // check that data is updated correctly
+        console.log(val);
+        console.log($scope.appInfo);
+      };
 
-        },{
-            name: "shaun",
-            belt:"green",
-            rate: 10,
-            available:false
-        },{
-            name: "jennie",
-            belt:"green",
-            rate: 30,
-            available:false
-        },{
-            name: "lima",
-            belt:"green",
-            rate:1000,
-            available:true
+    $scope.appInfo = {
+        personalInfo: {
+            flags:[],
+            firstName:null,
+            lastName:null,
+            email:null,
+            birthday:null,
         },
-    ]
+        legalStat: {
+            flags:[],
+            legalStatus:null,
+            drivers : {
+             hasDrivers:null,
+             driversType:null,
+            },
+            numPracticeHours:null,
+            prevPRAAttempts: [],
+        },
+       //written TDM, result, is current?
+       medicalEd: {
+        flags:[],
+        school:null,
+        medDegreeName:null,
+        gradYr:null,
+        edLang:null,
+       },
+       engProficiency:{
+        flags:[],
+        test:null,
+        score:null,
+        expired:null,
+        exceeded:null,
+        activeUse:null
+       },
+       exams:{
+        flags:[],
+        NACDate:null,
+        MCCQE2Date:null,
+        MCCQE1Date:null,
+       },
+       postGradTraining:{
+        flags:[],
+        monthsPostGrad:null,
+        monthsIndependent:null,
+        completed2Yr:null
+       },
+       rotations:{
+        flags:[],
+        completed7:null,
+        impairment:null
+       }
+    }
+
+    $scope.addPrevPRA = () => {
+        $scope.checkVal(1)
+        console.log("Added PRA")
+        $scope.appInfo.legalStat.prevPRAAttempts.push({
+            written:null,
+            passed:null,
+            current:null
+        })
+    }
+    $scope.delPrevPRA = (i:number) => {
+        $scope.checkVal(1)
+
+        console.log("Deleted PRA")
+        $scope.appInfo.legalStat.prevPRAAttempts.splice(i, 1);
+    }
+
     $scope.engProficiency = [
         "0 - No experience",
         "1 - Beginner",
@@ -71,6 +148,14 @@ touchstoneTest.controller('touchstoneTestController', ['$scope', function($scope
     $scope.immgStat = [
         "Canadian Citizen / Permeaneant Resident",
         "Neither"
+    ]
+    $scope.trueFalse = [true, false];
+    $scope.driversType = [
+        "Canadian",
+        "International"
+    ]
+    $scope.engTest = [
+        "IELTS", "OET", "CELPIP", "Recent practice in English speaking country"
     ]
 
 }]);
